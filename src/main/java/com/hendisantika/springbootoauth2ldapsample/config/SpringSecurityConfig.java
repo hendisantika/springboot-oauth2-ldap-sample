28dfc02b4e620c8f17534d1da10e71e7f3404766
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-03 04:47:48 +0700

    #12 - Add SpringSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootoauth2ldapsample/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootoauth2ldapsample.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-ldap-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/05/20
+ * Time: 04.47
+ */
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+}
