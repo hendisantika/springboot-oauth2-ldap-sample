cb15020e3d9226e5db271c3b99e7dc023d70a322
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-03 04:39:54 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootoauth2ldapsample/SpringbootOauth2LdapSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2ldapsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2LdapSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2LdapSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootoauth2ldapsample/SpringbootOauth2LdapSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2ldapsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2LdapSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
