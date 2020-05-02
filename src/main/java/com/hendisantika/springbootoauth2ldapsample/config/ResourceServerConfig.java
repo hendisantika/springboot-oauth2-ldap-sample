3d9c271319b32d0892221a81e7a5d9ac2cb2680e
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-03 04:45:52 +0700

    #9 - Add configure Bean in ResourceServerConfig

--- src/main/java/com/hendisantika/springbootoauth2ldapsample/config/ResourceServerConfig.java
+++ src/main/java/com/hendisantika/springbootoauth2ldapsample/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootoauth2ldapsample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -16,5 +17,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/webjars/**").permitAll().anyRequest().authenticated().and().cors()
+                .configurationSource(corsConfigurationSource());
 
+    }
 }
